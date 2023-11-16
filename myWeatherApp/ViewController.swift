6a319ca18be8da5619515224654e6d5984ba60ac
H8K-iOS <alexalimov17@gmail.com>
2023-11-16 12:38:10 +0100

    Initial Commit

---
+++ myWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myWeatherApp
+//
+//  Created by Alexandr Alimov on 16/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
